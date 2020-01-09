b6bfd5b04af6fc83d03cc680bd2226cb661e2ad0
colledkm <colledkm@rose-hulman.edu>
2020-01-09 13:14:54 -0500

    Added in direction and speed

--- moveDist.py
+++ moveDist.py
@@ -4,20 +4,34 @@
 
 def movedist (distance, direction,speed,device):
     from zaber.serial import BinarySerial, BinaryDevice, BinaryCommand, BinaryReply
+    from check_command_succeeded import check_command_succeeded
     import time
-    # -------------------------------------------------------------------------
-    # TODO a: Convert distances to steps.  Put in the true conversion.
-    # -------------------------------------------------------------------------
-
-        converter = 1 #converts distance to steps.  (steps/distance)
-        steps = distance*converter #number of microsteps to move
 
     # -------------------------------------------------------------------------
-    # TODO a: Insert motor move command
+    # TODO: Convert direction into +1 or -1  Put in the true conversion.
     # -------------------------------------------------------------------------
+
+    if direction=="R":
+        direction_sign = 1 #rotate clockwise
+    else:
+        direction_sign = -1 #rotate counterclockwise
+
+    # -------------------------------------------------------------------------
+    # TODO: Convert distances to steps.  Put in the true conversion.
+    # -------------------------------------------------------------------------
+
+    converter = 1 #converts distance to steps.  (steps/distance)
+    steps = distance*converter*direction_sign #number of microsteps to move
+
+    # -------------------------------------------------------------------------
+    # TODO: Insert motor move command
+    # -------------------------------------------------------------------------
+    device.move_rel(speed,blocking=True)
     reply = device.move_rel(steps)  # move rel 2000 microsteps
+
     if not check_command_succeeded(reply):
         print("Device move failed.")
         exit(1)
-    else print("Motor moved", distance, "microsteps")
+    else:
+        print("Motor moved", distance, "microsteps")
     return;
