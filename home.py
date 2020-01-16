090cf306fd475e934401cd5540d4b47ff49f98f6
colledkm <colledkm@rose-hulman.edu>
2020-01-16 12:55:52 -0500

    Fixing errors with home.py

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 def home(device):
 
     from zaber.serial import BinarySerial, BinaryDevice, BinaryCommand, BinaryReply
-    from check_command_check import check_command_check
+    from check_command_succeeded import check_command_succeeded
 
     reply = device.home()
     if check_command_succeeded(reply):
@@ -9,4 +9,4 @@ def home(device):
     else:
         print("Device home failed.")
         exit(1)
-return;
+return
