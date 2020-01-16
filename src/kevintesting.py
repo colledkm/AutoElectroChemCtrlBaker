c880104584b5f2f189dac3defdfa0445fde3559f
colledkm <colledkm@rose-hulman.edu>
2020-01-16 14:42:09 -0500

    link for kevin

--- src/kevintesting.py
+++ src/kevintesting.py
@@ -1,1 +1,3 @@
-Here's an area where you can do code testing with out it impacting the rest of the functions!  Enjoy!
+#Here's an area where you can do code testing with out it impacting the rest of the functions!  Enjoy!
+
+# go here for help: https://www.rose-hulman.edu/class/csse/csse120/201920/
