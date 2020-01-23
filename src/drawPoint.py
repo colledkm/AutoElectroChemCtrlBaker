65fba3f70bdcab760f8bf54bebd1567a8b0e9e58
colledkm <colledkm@rose-hulman.edu>
2020-01-23 13:01:42 -0500

    Finished draw Point function

--- src/drawPoint.py
+++ src/drawPoint.py
@@ -2,6 +2,8 @@ def drawPoint(self, pointLoc):
     import rosegraphics as rg
 
     self.go_to(pointLoc)
-    turtle.pen_down()
-    turtle.draw_circle(4)
-    turtle.pen_up()
+    self.pen_down()
+    self.draw_circle(4)
+    self.pen_up()
+
+    return;
