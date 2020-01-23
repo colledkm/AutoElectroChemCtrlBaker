3700e7a6deffed3fd3498012816d0af5778e84ac
colledkm <colledkm@rose-hulman.edu>
2020-01-23 10:43:23 -0500

    fixed for loop and working on making turtles

--- src/turtle_test.py
+++ src/turtle_test.py
@@ -2,6 +2,29 @@ import rosegraphics as rg
 
 window = rg.TurtleWindow()
 
-blue_turtle = rg.SimpleTurtle('turtle')
-blue_turtle.pen = rg.Pen('midnight blue')
+horDist = 50
+verDist = 50
+nrow = 6 # number of rows
+ncol = 6 # number of columns
 
+turtle = rg.SimpleTurtle()
+
+# put the turtle pen at home
+turtle.pen = rg.Pen("red",2)
+turtle.pen_up()
+home = rg.Point(-100,100)
+turtle.go_to(home)
+
+# move across the row
+
+for r in range(ncol):
+    turtle.pen_down()
+    turtle.draw_circle(4)
+    turtle.pen_up()
+    turtle.forward(horDist)
+
+#for r in nrow:
+#    turtle.forward(verDist)
+
+
+window.close_on_mouse_click()
