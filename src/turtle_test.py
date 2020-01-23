b52d0980be74d0e2dfb6c80f9e4ea6e317f0f50a
colledkm <colledkm@rose-hulman.edu>
2020-01-23 13:35:30 -0500

    turtle_test is working for normal matrix

--- src/turtle_test.py
+++ src/turtle_test.py
@@ -4,7 +4,7 @@ from drawPoint import drawpoint
 window = rg.TurtleWindow()
 
 horDist = 50
-verDist = 50
+verDist = 100
 nrow = 3 # number of rows
 ncol = 3 # number of columns
 
@@ -13,29 +13,23 @@ turtle = rg.SimpleTurtle()
 # put the turtle pen at home
 turtle.pen = rg.Pen("red",2)
 home = rg.Point(-100,100)
-drawpoint(turtle,home)
-
 
 # move across the row
-Loc = home
-Loc.x = turtle.x_cor()+50
+Loc = rg.Point(turtle.x_cor(),turtle.y_cor())
+for c in range(ncol):
+   # drawpoint(turtle, Loc)
+    if c%2 == 0:
+        for r in range(nrow):
+            drawpoint(turtle,Loc)
+            if r != nrow-1:
+                Loc.x = turtle.x_cor()+horDist
 
-
-#turtle.right(90)
-#turtle.forward(verDist)
-#turtle.right(90)
-#turtle.forward(horDist)
-
-#for r in range(nrow):
-#    turtle.pen_down()
-#    turtle.draw_circle(4)
-#    turtle.pen_up()
-#    turtle.forward(horDist)
-
-#turtle.right(-90)
-#turtle.forward(verDist)
-#turtle.right(-90)
-#turtle.forward(horDist)
+    else:
+        for r in range(nrow):
+            drawpoint(turtle,Loc)
+            if r != nrow-1:
+                Loc.x = turtle.x_cor()-horDist
+    Loc.y = turtle.y_cor()-verDist
 
 
 window.close_on_mouse_click()
