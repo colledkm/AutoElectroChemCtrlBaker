e3e625c603870d944d95d3158d8ea871d6acc24b
colledkm <colledkm@rose-hulman.edu>
2020-01-30 13:31:08 -0500

    working on setting up gui

--- src/Gui_test.py
+++ src/Gui_test.py
@@ -1,11 +1,147 @@
 import tkinter
 from tkinter import ttk
 
-def main():
-    root=tkinter.Tk()
-    frame = ttk.Frame(root, padding=20)
-    frame.grid()
-    button = ttk.Button(frame, text="Say Hello")
+def directCtrlFrame(root):
 
+    main_frame = ttk.Frame(root, padding=20)
+    main_frame.grid()  # only grid call that does NOT need a row and column
+
+    left_speed_label = ttk.Label(main_frame, text="Left Speed")
+    left_speed_label.grid(row=0, column=0)
+    left_speed_entry = ttk.Entry(main_frame, width=8)
+    left_speed_entry.insert(0, "600")
+    left_speed_entry.grid(row=1, column=0)
+
+    step_numb_label = ttk.Label(main_frame, text="Number of Steps")
+    step_numb_label.grid(row=0, column=1)
+    step_numb_entry = ttk.Entry(main_frame, width=8)
+    step_numb_entry.insert(0, "20")
+    step_numb_entry.grid(row=1, column=1)
+
+    right_speed_label = ttk.Label(main_frame, text="Right Speed")
+    right_speed_label.grid(row=0, column=2)
+    right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
+    right_speed_entry.insert(0, "600")
+    right_speed_entry.grid(row=1, column=2)
+
+    forward_button = ttk.Button(main_frame, text="Forward")
+    forward_button.grid(row=3, column=1)
+    forward_button['command'] = lambda: print("Forward button")
+    root.bind('<Up>', lambda event: print("Forward key"))
+
+    left_button = ttk.Button(main_frame, text="Left")
+    left_button.grid(row=4, column=0)
+    left_button['command'] = lambda: print("Left button")
+    root.bind('<Left>', lambda event: print("Left key"))
+
+    stop_button = ttk.Button(main_frame, text="Stop")
+    stop_button.grid(row=4, column=1)
+    stop_button['command'] = lambda: print("Stop button")
+    root.bind('<space>', lambda event: print("Stop key"))
+
+    right_button = ttk.Button(main_frame, text="Right")
+    right_button.grid(row=4, column=2)
+    right_button['command'] = lambda: print("Right button")
+    root.bind('<Right>', lambda event: print("Right key"))
+
+    back_button = ttk.Button(main_frame, text="Back")
+    back_button.grid(row=5, column=1)
+    back_button['command'] = lambda: print("Back button")
+    root.bind('<Down>', lambda event: print("Back key"))
+
+    up_button = ttk.Button(main_frame, text="Up")
+    up_button.grid(row=6, column=0)
+    up_button['command'] = lambda: print("Up button")
+    root.bind('<u>', lambda event: print("Up key"))
+
+    down_button = ttk.Button(main_frame, text="Down")
+    down_button.grid(row=7, column=0)
+    down_button['command'] = lambda: print("Down button")
+    root.bind('<j>', lambda event: print("Down key"))
+
+    # Buttons for quit and exit
+    q_button = ttk.Button(main_frame, text="Quit")
+    q_button.grid(row=6, column=2)
+    q_button['command'] = lambda: print("Quit button")
+
+    e_button = ttk.Button(main_frame, text="Exit")
+    e_button.grid(row=7, column=2)
+    e_button['command'] = lambda: exit()
+
+def matrixFrame(root):
+
+    main_frame = ttk.Frame(root, padding=20)
+    main_frame.grid()  # only grid call that does NOT need a row and column
+
+    left_speed_label = ttk.Label(main_frame, text="Left Speed")
+    left_speed_label.grid(row=0, column=0)
+    left_speed_entry = ttk.Entry(main_frame, width=8)
+    left_speed_entry.insert(0, "600")
+    left_speed_entry.grid(row=1, column=0)
+
+    step_numb_label = ttk.Label(main_frame, text="Number of Steps")
+    step_numb_label.grid(row=0, column=1)
+    step_numb_entry = ttk.Entry(main_frame, width=8)
+    step_numb_entry.insert(0, "20")
+    step_numb_entry.grid(row=1, column=1)
+
+    right_speed_label = ttk.Label(main_frame, text="Right Speed")
+    right_speed_label.grid(row=0, column=2)
+    right_speed_entry = ttk.Entry(main_frame, width=8, justify=tkinter.RIGHT)
+    right_speed_entry.insert(0, "600")
+    right_speed_entry.grid(row=1, column=2)
+
+    forward_button = ttk.Button(main_frame, text="Forward")
+    forward_button.grid(row=3, column=1)
+    forward_button['command'] = lambda: print("Forward button")
+    root.bind('<Up>', lambda event: print("Forward key"))
+
+    left_button = ttk.Button(main_frame, text="Left")
+    left_button.grid(row=4, column=0)
+    left_button['command'] = lambda: print("Left button")
+    root.bind('<Left>', lambda event: print("Left key"))
+
+    stop_button = ttk.Button(main_frame, text="Stop")
+    stop_button.grid(row=4, column=1)
+    stop_button['command'] = lambda: print("Stop button")
+    root.bind('<space>', lambda event: print("Stop key"))
+
+    right_button = ttk.Button(main_frame, text="Right")
+    right_button.grid(row=4, column=2)
+    right_button['command'] = lambda: print("Right button")
+    root.bind('<Right>', lambda event: print("Right key"))
+
+    back_button = ttk.Button(main_frame, text="Back")
+    back_button.grid(row=5, column=1)
+    back_button['command'] = lambda: print("Back button")
+    root.bind('<Down>', lambda event: print("Back key"))
+
+    up_button = ttk.Button(main_frame, text="Up")
+    up_button.grid(row=6, column=0)
+    up_button['command'] = lambda: print("Up button")
+    root.bind('<u>', lambda event: print("Up key"))
+
+    down_button = ttk.Button(main_frame, text="Down")
+    down_button.grid(row=7, column=0)
+    down_button['command'] = lambda: print("Down button")
+    root.bind('<j>', lambda event: print("Down key"))
+
+    # Buttons for quit and exit
+    q_button = ttk.Button(main_frame, text="Quit")
+    q_button.grid(row=6, column=2)
+    q_button['command'] = lambda: print("Quit button")
+
+    e_button = ttk.Button(main_frame, text="Exit")
+    e_button.grid(row=7, column=2)
+    e_button['command'] = lambda: exit()
+
+def main():
+    root = tkinter.Tk()
+
+    directCtrlFrame(root)
+
+    matrixFrame(root)
+
+    root.mainloop()
 
 main()
