dd6341f94756820bbfce20aa8a2359626d712339
Kevin Chou <chouk@rose-hulman.edu>
2020-01-30 01:50:49 -0500

    test success

--- src/kevintesting.py
+++ src/kevintesting.py
@@ -10,16 +10,17 @@
 # ncol = 5
 
 from zaber.serial import BinarySerial, BinaryDevice, BinaryCommand  # , BinaryReply
-# import time
+import time
 
 # Assume that our serial port can be found at COM1
 
-port = BinarySerial("COM13")
+port = BinarySerial("COM13")    # be mindful of which COM port to use; check device manager; may need to restart port
 # with BinarySerial("COM13") as port:
-# Device number 0, command number 1.
-command = BinaryCommand(0, 1)
-# note: Binary motor commands are blocking
-port.write(command)
+# Device number 0, command number 1.    1 is Home. 0 is Reset, move absolute is 20, relative is 21
+# command = BinaryCommand(0, 1)
+# note: Binary motor commands are blocking. Syntax: BinaryCommand(device#, command#, dataValue)
+# List of command #s can be found at: https://www.zaber.com/wiki/Manuals/Binary_Protocol_Manual#Command_Reference
+# port.write(command)
 
 # Assume that we have 1 devices connected.
 ndevices = 1
@@ -30,5 +31,8 @@ ndevices = 1
 # Assume we have a BinarySerial object called "port".
 device1 = BinaryDevice(port, 1)
 
-device1.move_abs(3000)
-# device1.move_rel(1000)
+# device1.move_abs(5000)
+device1.home()          # don't recommend using device.functions because only certain common commands have functions & send oesn't seem to work
+time.sleep(5)
+# device1.move_rel(5000)
+# device1.send("home")    # doesn't work
