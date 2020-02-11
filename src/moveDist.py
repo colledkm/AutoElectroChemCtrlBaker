55ac647c9d260d9f1874aaa8a022bbfbed80df0c
Kevin Chou <chouk@rose-hulman.edu>
2020-02-11 01:49:24 -0500

    test success

--- src/moveDist.py
+++ src/moveDist.py
@@ -3,43 +3,45 @@
 # -------------------------------------------------------------------------
 
 def movedist (distance, direction,device):
-    #removed speed for now (third variable)
-    #where device = device number.
+    # removed speed for now (third variable)
+    # where device = device number.
     from src.check_command_succeeded import check_command_succeeded
     from zaber.serial import BinarySerial, BinaryDevice, BinaryCommand  # , BinaryReply #Added
-    import time #Added
+    import time # Added
 
     # -------------------------------------------------------------------------
     # TODO: Convert direction into +1 or -1  Put in the true conversion.
     # -------------------------------------------------------------------------
 
-    if direction==1:  #Changed from R to E for extend since + = extend - = retract in commands
-        direction_sign = 1 #extend
+    if direction == 1:  # Changed from R to E for extend since + = extend - = retract in commands
+        direction_sign = 1 # extend
     else:
-        direction_sign = -1 #retract
+        direction_sign = -1 # retract
 
     # -------------------------------------------------------------------------
     # TODO: Convert distances to steps.  Put in the true conversion.
     # -------------------------------------------------------------------------
 
-    converter = 1 #converts distance to steps.  (steps/distance)
+    converter = 1 # converts distance to steps.  (steps/distance)
     steps = distance*converter*direction_sign #number of microsteps to move
 
     # -------------------------------------------------------------------------
     # TODO: Insert motor move command
     # -------------------------------------------------------------------------
-    #device.move_rel(speed,blocking=True)   #Added Commented Out
-               #Added
-    #command = BinaryCommand(device, 21, steps)   #Added. 21 = move rel
-    port = BinarySerial("COM13")  # Added
+    # device.move_rel(speed,blocking=True)   # Added Commented Out
+
+    # command = BinaryCommand(device, 21, steps)   # Added. 21 = move rel
+    port = BinarySerial("COM13")                  # Added
     command = BinaryCommand(0, 1)
-    port.write(command)                     #Added
+    port.write(command)                           # Added
 
-    #reply = device.move_rel(steps)  # move rel 2000 microsteps #Added Commented out everything below
+    #r eply = device.move_rel(steps)  # move rel 2000 microsteps #A dded Commented out everything below
 
-    #if not check_command_succeeded(reply):
+    # if not check_command_succeeded(reply):
         #print("Device move failed.")
         #exit(1)
-    #else:
-        #print("Motor moved", distance, "microsteps")
-    #return
+    # else:
+       # print("Motor moved", distance, "microsteps")
+    return
+
+movedist(10000,1,"COM13")
